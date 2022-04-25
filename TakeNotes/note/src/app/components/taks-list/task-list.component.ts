fe3e0d94c95af0211d3f3a1acb059af713dae133
Joseph <king_joseph@live.com>
2022-04-25 17:47:58 +0100

    Create TakeNotes
    Adding an old repo

---
+++ TakeNotes/note/src/app/components/taks-list/task-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from '../../interfaces/task';
+
+@Component({
+  selector: 'task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.less']
+})
+export class taskListComponent implements OnInit {
+    tasks : Task[];
+    taskTitle: string;
+    idForTasks : number;
+    completed : boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+    this.idForTasks = 2;
+    this.taskTitle = '';
+    this.tasks = [
+    {
+      'id' : 1,
+      'title' : 'Finsh angular screenshot',
+      'completed' : false,
+      'editing' : false
+    },
+
+  ];
+
+}
+
+  addTask(): void{
+    if(this.taskTitle.trim().length == 0 ){
+      return;
+    }
+
+    this.tasks.push({
+      id: this.idForTasks,
+      title: this.taskTitle,
+      completed:false,
+      editing:false
+    })
+
+    this.taskTitle = '';
+    this.idForTasks++;
+
+  }
+
+  deleteTask(id:number):void{
+    this.tasks = this.tasks.filter(task => task.id !==id); 
+  }
+
+  remaining(): number {
+    return this.tasks.filter(task => !task.completed).length;
+  }
+
+  done(): number {
+    return this.tasks.filter(task => task.completed).length;
+  }
+
+}
